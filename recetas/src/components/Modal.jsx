e96b65312cc3fd1c09ac7f2c73597ffb5164cd2a
RichyRed <richyred1090@gmail.com>
2023-05-31 15:12:55 -0400

    arreglitos

--- recetas/src/components/Modal.jsx
+++ recetas/src/components/Modal.jsx
@@ -4,8 +4,8 @@ const Modal = ({ recetas }) => {
   const count = recetas.reduce((total, receta) => total + 1, 0);
 
   return (
-    <div className="modal">
-      <p>Total de recetas: {count}</p>
+    <div style={{ backgroundColor: 'white', padding: '20px', borderRadius: '8px', boxShadow: '0 2px 8px rgba(0, 0, 0, 0.15)', textAlign: 'center', position: 'fixed', bottom: 0, left: 0, width: '100%' }}>
+      <p style={{ fontWeight: 'bold' }}>Total de recetas: {count}</p>
     </div>
   );
 };
